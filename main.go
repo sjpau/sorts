5ac4d03173d0a7877bf93606a51e243f3c4b19ef
Jan Sauchyk <mobyus@mobyus.xyz>
2022-07-16 22:25:50 +0300

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	ebiten.SetWindowResizable(true)
+	ebiten.SetFPSMode(ebiten.FPSModeVsyncOffMaximum)
+	if err := ebiten.RunGame(NewGame()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewGame() *Game {
+	return &Game{}
+}
