3787c0e795ed3ad35910a34e52d1865ab2d9b14e
Jan Sauchyk <mobyus@mobyus.xyz>
2022-07-26 08:55:37 +0300

    optimizing bubble sort

--- algorithm/bubble.go
+++ algorithm/bubble.go
@@ -1,6 +1,7 @@
 package algorithm
 
 import (
+	"fmt"
 	"sort"
 	"time"
 )
@@ -9,7 +10,6 @@ func Bubble(arr []int, delay time.Duration) {
 	ticker := time.NewTicker(delay)
 	done := make(chan bool)
 	i := 0
-	j := 0
 	length := len(arr)
 	go func() {
 		for {
@@ -17,22 +17,18 @@ func Bubble(arr []int, delay time.Duration) {
 			case <-done:
 				return
 			case <-ticker.C:
-				if i > length-1 {
+				if i >= length-1 {
 					i = 0
 				} else {
 					i++
 				}
-				if j == length-i-1 {
-					j = 0
-				} else {
-					j++
-				}
 			}
+			fmt.Println(i)
 		}
 	}()
 	for !sort.IntsAreSorted(arr) {
-		if j != length-1 && arr[j] > arr[j+1] {
-			arr[j], arr[j+1] = arr[j+1], arr[j]
+		if i != length-1 && arr[i] > arr[i+1] {
+			arr[i], arr[i+1] = arr[i+1], arr[i]
 		}
 	}
 	done <- true
