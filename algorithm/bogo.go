734a47632d7034001f835b530718a54c14dbbb9a
Jan Sauchyk <mobyus@mobyus.xyz>
2022-07-30 21:34:35 +0300

    You can't stop the greatest algorithm of ALL TIME! NOBODY CAN!

--- algorithm/bogo.go
+++ algorithm/bogo.go
@@ -4,18 +4,14 @@ import (
 	"sort"
 	"time"
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/sjpau/sorts/misc"
 )
 
 /*simple and elegant... yet efficient!*/
 func Bogo(arr []int, delay time.Duration) {
+
 	for !sort.IntsAreSorted(arr) {
-		if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
-			break
-		}
 		misc.Shuffle(arr)
-		time.Sleep(delay * 20)
+		time.Sleep(delay * 25)
 	}
 }
